1f0ab355603b75d5491b0ba478a379cc411f242a
Vladislav <v.kirbaba@gmail.com>
2016-07-08 13:08:13 +0300

    Модалка. главная

--- app/js/modal.js
+++ app/js/modal.js
@@ -6,6 +6,7 @@ jQuery(document).ready(function($) {
 		$("." + modal).append('<div class="modal_active"></div>');
 		$(".modal__box").slideDown().addClass('modal__box_visible');
 	});
+
 	$(document).on("click", ".modal_active", function(event) {
 		event.preventDefault();
 		$(".modal__box").slideUp().removeClass('modal__box_visible');
@@ -22,6 +23,26 @@ jQuery(document).ready(function($) {
 		$(".modal").hide();
 	});
 
+	$(document).on("click", ".home-projects__tabs_item", function(event) {
+		event.preventDefault();
+		var modal = ($(this).data('modal'));
+		$("." + modal).show(500);
+		$(".modal__box").slideDown().addClass('modal__box_visible');
+		var image = $(this).attr("href");
+		console.log('image: ' + image);
+		$(".modal__project_image-box").append('<img alt="" />').delay(2000);
+		$(".modal__project_image-box img").attr("src", image);
+		var content = $(this).children(".home-projects__tabs_content").html();
+		$(".modal__project_content").append('<div class="modal__project_content_wrap">' + content + '</div>').delay(2000);
+	});
+
+	$(document).on("click", ".modal__project_close", function(event) {
+		event.preventDefault();
+		$(".modal__box").slideUp().removeClass('modal__box_visible');
+		$(".modal").hide(200);
+		$(".modal__project_image-box img").remove();
+		$(".modal__project_content_wrap").remove();
+	});
 
 	 $(document).keydown(function(e) {
 	 	if(".modal".length != 0) {
--- header.php
+++ header.php
@@ -26,4 +26,45 @@
 	<?php wp_head(); // необходимо для работы плагинов и функционала ?>
 </head>
 <body <?php body_class(); // все классы для body ?>>
+	<!-- open .navigation -->
+	<nav class="navigation">
+		<!-- open .container -->
+		<div class="container">
+			<!-- open .navigation__logo -->
+			<div class="navigation__logo">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="-307.5 347.6 137.2 17">
+				  <style>
+				    .st0{fill:#FFFFFF;} .st1{fill:#42FFEB;}
+				  </style>
+				  <path class="st0" d="M-296.8 361.8h-5.8l-.8 2.6h-4.1l5.4-16.5h4.8l5.4 16.5h-4.1l-.8-2.6zm-1.1-3.5l-1.8-6.2-1.8 6.2h3.6zm14.9.7h-1.5v5.4h-3.8v-16.5h6.4c3.1 0 5.6 2.5 5.6 5.6 0 2-1.2 3.8-2.9 4.7l4.8 6.1h-4.4l-4.2-5.3zm-1.4-3.3h2.6c1 0 1.9-.9 1.9-2.1s-.8-2.1-1.9-2.1h-2.6v4.2zm22.5-4.1h-3.8v12.8h-3.8v-12.8h-3.8V348h11.3l.1 3.6zm11.6 4.6c0-4.8 3.6-8.6 8.6-8.6 4 0 7.4 2.7 8.1 6.4h-3.9c-.5-1.7-2.2-2.8-4.1-2.8-2.9 0-4.8 2-4.8 4.9s1.9 4.9 4.8 4.9c2 0 3.6-1.1 4.1-2.8h3.9c-.7 3.8-4 6.4-8.1 6.4-5 .1-8.6-3.6-8.6-8.4zm25.8 2.8h-1.5v5.4h-3.8v-16.5h6.4c3.1 0 5.6 2.5 5.6 5.6 0 2-1.2 3.8-2.9 4.7l4.8 6.1h-4.4l-4.2-5.3zm-1.4-3.3h2.6c1 0 1.9-.9 1.9-2.1s-.8-2.1-1.9-2.1h-2.6v4.2zm22.6 6.1h-5.8l-.8 2.6h-4.1l5.4-16.5h4.8l5.4 16.5h-4.1l-.8-2.6zm-1-3.5l-1.8-6.2-1.8 6.2h3.6zm13.4-6.7v3.4h5.9v3.6h-5.9v5.8h-3.8v-16.5h9.8v3.6h-6v.1zm20.6 0h-3.8v12.8h-3.8v-12.8h-3.8V348h11.3v3.6h.1z"/>
+				  <path class="st1" d="M-262.3 355h16.1v2.7h-16.1z"/>
+				</svg>
+			</div>
+			<!-- open .navigation__menu_toggle -->
+			<a href="#" class="navigation__menu_toggle">
+				<span></span>
+			</a>
+			<!-- close .navigation__menu_toggle -->
+			<!-- close .navigation__logo -->
+			<ul class="navigation__menu">
+				<li><a href="/" id="moveToTop">Главная</a></li>
+				<li><a data-scroll href="#moveto_projects">Проекты</a></li>
+				<li><a data-scroll href="#moveto_about">Акции</a></li>
+			</ul>
 
+			<!-- open .navigation__lang -->
+			<div class="navigation__lang">
+				<a href="#nowhere" class="navigation__lang_active">ru </a>
+				<!-- open .navigation__lang -->
+				<ul class="navigation__lang_list">
+					<li><a href="#nowhere" class="navigation__lang_val" data-lang="en">en</a></li>
+					<li><a href="#nowhere" class="navigation__lang_val" data-lang="ru">ru</a></li>
+				</ul>
+				<!-- close .navigation__lang -->
+			</div>
+			<!-- close .navigation__lang -->
+
+		</div>
+		<!-- close .container -->
+	</nav>
+	<!-- close .navigation -->
